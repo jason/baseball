3620fa93203b1b5531ca85b0e84e9b349afb8c58
Jason Wong <technique@gmail.com>
2013-01-29 17:48:36 -0800

    added most experienced method

--- baseball/app/models/person.rb
+++ baseball/app/models/person.rb
@@ -11,14 +11,24 @@ class Person < ActiveRecord::Base
   has_many :management_lines, :foreign_key => "managerID", :primary_key => "managerID"
   has_many :managed_teams, :through => :management_lines, :source => :team
 
-  def self.niceBattingAverage
+  def self.niceBattingAverage(avg)
     self
       .select("Master.playerID")
       .joins(:batting_lines)
-      .where("(batting.H / batting.AB) > 0.400 AND batting.AB > 100")
+      .where("(batting.H / batting.AB) > #{avg} AND batting.AB > 100")
       .group("playerID")
   end
 #Person.select("Master.playerID").joins(:batting_lines).where("(batting.H / batting.AB) > 0.400 AND batting.AB > 100").group("playerID")
 #select (H / AB), playerID from Batting where AB > 100 AND (H / AB) > 0.400 GROUP BY playerID;
 
+  def self.most_experienced
+    self
+    .select("Master.nameFirst, Master.nameLast, Master.playerID, COUNT(DISTINCT yearID) as exp")
+    .joins(:playerships)
+    .group("playerID")
+    .order("exp DESC")
+    .limit(1)
+  end
+
+# select playerid, COUNT(DISTINCT yearID) as exp FROM Appearances GROUP BY playerID ORDER BY exp DESC LIMIT 1;
 end
